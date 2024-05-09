aa7a66b8ad7d3944b8c8b23c3b55fda83e807fb6
Mukhamedyakub <230198@astanait.edu.kz>
2024-05-09 20:34:31 +0500

    Created part_1_2 Student class

---
+++ Part_1_2/Student.java
@@ -0,0 +1,17 @@
+package Part_1_2;
+
+/**
+ * Represents a student.
+ */
+class Student {
+    private String studentName;
+
+    /**
+     * Constructor for Student.
+     *
+     * @param studentName the name of the student
+     */
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+}
